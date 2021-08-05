384c0e045cf7bbdb90838e327e507db55f05ced1
Nitin Takshak <nitintaxak@gmail.com>
2021-08-05 10:42:03 +0530

    commit from america

--- src/First.java
+++ src/First.java
@@ -6,7 +6,13 @@ public class First
 	public void one()
 	{
 		System.out.println("One");
+		System.out.println("One more one");
 	}
-	
+	@Test
+	public void second()
+	{
+		System.out.println("Second");
+		System.out.println("One more second");
+	}
 
 }
